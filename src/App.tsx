681d7d0b6b3c6d6cc6f143ed015689756532e0d1
Alex <alexkazakqq@gmai.com>
2022-10-22 22:14:59 +0300

    counter version 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
+import "./App.css";
+import {Container} from "./components/Container/Container";
+import {Setting} from "./components/Setting/Setting";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    // const [maxValue, setMaxValue] = useState(0);
+    // const [startValue, setStartValue] = useState(0);
+    // const [counter, setCounter] = useState(0);
+    // const [isSetting,setIsSetting] = useState(false);
+
+    // const getParams = () => {
+    //     const max = localStorage.getItem('maxValue')
+    //     if (max){
+    //         setMaxValue(JSON.parse(max));
+    //     }
+    //     const start = localStorage.getItem('startValue')
+    //     if (start) {
+    //         setStartValue(JSON.parse(start))
+    //     }
+    //     const count = localStorage.getItem('counter')
+    //     if (count) {
+    //         setCounter(JSON.parse(count))
+    //     }
+    // }
+
+    // useEffect(() => {
+    // //     getParams()
+    // // },[])
+
+    const [maxValue, setMaxValue] = useState(JSON.parse(localStorage.getItem('maxValue') || '0'));
+    const [startValue, setStartValue] = useState(JSON.parse(localStorage.getItem('startValue') || '0'));
+    const [counter, setCounter] = useState(JSON.parse(localStorage.getItem('counter') || '0'));
+    const [isSetting,setIsSetting] = useState(false);
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+
+        localStorage.setItem('counter', JSON.stringify(counter))
+        localStorage.setItem('maxValue', JSON.stringify(maxValue))
+        localStorage.setItem('startValue', JSON.stringify(startValue))
+    }, [counter, maxValue, startValue])
+
+
+    return (
+        <div className="App">
+            <Setting
+                isSetting={isSetting}
+                setMaxValue={setMaxValue}
+                setIsSetting={setIsSetting}
+                setStartValue={setStartValue}
+                counter={counter}
+                startValue={startValue}
+                maxValue={maxValue}
+                setCounter={setCounter}
+                setError={setError}
+            />
+            <Container
+                counter={counter}
+                isSetting={isSetting}
+                setCounter={setCounter}
+                maxValue={maxValue}
+                startValue={startValue}
+                error={error}
+            />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Setting/StartValue/StartValue.tsx
@@ -0,0 +1,47 @@
+import React, {ChangeEvent} from "react";
+import s from "./StartValue.module.css"
+
+type StartValuePropsType = {
+    setMaxValue?: (maxValue:number) => void
+    setStartValue?: (startValue:number) => void
+    setIsSetting: (isSetting:boolean) => void
+    startValue: number
+    maxValue: number
+    setError: (error:boolean) => void
+}
+
+export const StartValue = (props: StartValuePropsType) => {
+
+    const onChangeHandlerMax = (e: ChangeEvent<HTMLInputElement>) => {
+        if (props.setMaxValue) {
+            props.setMaxValue(+e.currentTarget.value)
+            props.setIsSetting(true);
+        }
+    }
+    const onChangeHandlerMin = (e: ChangeEvent<HTMLInputElement>) => {
+        if (props.setStartValue) {
+            props.setStartValue(+e.currentTarget.value)
+            props.setIsSetting(true);
+        }
+    }
+
+    props.setError(props.maxValue <= props.startValue)
+
+    let inputClass = props.maxValue <= props.startValue ? s.error : ''
+
+
+    return (
+        <div className={s.board}>
+            <div className={s.text}>
+                <span>max value:</span>
+                <input className={inputClass} value={props.maxValue}  min={0} type="number"   onChange={onChangeHandlerMax}></input>
+            </div>
+            <div className={s.text}>
+                <span>start value:</span>
+                <input className={inputClass} value={props.startValue} min={0} type="number"  onChange={onChangeHandlerMin}></input>
+            </div>
+
+        </div>
+    );
+};
+
